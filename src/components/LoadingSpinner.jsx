a4298263e141b8b600045b42c9fdf89bac492794
Ramtin <77804814+RamtinPRG@users.noreply.github.com>
2022-01-06 06:08:34 +0330

    Delete src directory

--- src/components/LoadingSpinner.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./styles/LoadingSpinner.css";
-
-function LoadingSpinner() {
-  return <i className="fas fa-spinner spinner"></i>;
-}
-
-export default LoadingSpinner;
